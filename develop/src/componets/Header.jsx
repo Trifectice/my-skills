6159d4e281a287dbab4e13550582558f78963d65
Scott Keller <smkdbv911@gmail.com>
2023-11-06 16:03:59 -0700

    adding boiler plate for header

---
+++ develop/src/componets/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>My Website</h1>
+      {/* Navigation component can be included here if it's part of the header */}
+      {/* <Navigation /> */}
+    </header>
+  );
+}
+
+export default Header;
